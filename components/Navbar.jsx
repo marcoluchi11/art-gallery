135e40ad57b1f90063b243a3fba73eee9327d033
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-12 16:02:49 -0300

    add modifications to look nice in both mobile and desktop

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,21 +3,21 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav>
-      <h1 className="text-3xl  text-center font-thin transition  duration-300 ease-in-out tracking-tight hover:tracking-wide	w-full  ">
+      <h1 className="text-8xl  text-center font-thin transition  duration-300 ease-in-out tracking-tight hover:tracking-wide	w-full  ">
         Albion Vuart
       </h1>
-      <ul className="flex flex-col md:flex-row justify-around font-thin text-blue-500 mt-10">
+      <ul className="flex md:flex-row justify-around font-thin text-blue-500 mt-10">
         <Link href="/" className="tracking-tight hover:tracking-wide">
-          <li>Home</li>
+          <li className="text-xl">Home</li>
         </Link>
         <Link href="/about" className="tracking-tight hover:tracking-wide">
-          <li>About</li>
+          <li className="text-xl">About</li>
         </Link>
         <Link href="/portfolio" className="tracking-tight hover:tracking-wide">
-          <li>Portfolio</li>
+          <li className="text-xl">Portfolio</li>
         </Link>
         <Link href="/contact" className="tracking-tight hover:tracking-wide">
-          <li>Contact</li>
+          <li className="text-xl">Contact</li>
         </Link>
       </ul>
     </nav>
