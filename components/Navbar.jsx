cfcd1f970df923ee63a9ebf6b9f868e11388b5ed
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-12 19:35:02 -0300

    add logo at the top

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,23 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav>
-      <h1 className="text-8xl  text-center font-thin transition  duration-300 ease-in-out tracking-tight hover:tracking-wide	w-full  ">
-        Albion Vuart
-      </h1>
+    <nav className="border-solid border-b-2 border-gray-300">
+      <div className="flex flex-col items-center">
+        <Link href="/">
+          <Image
+            src="/logo.jpg"
+            alt="logo"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+        </Link>
+        <h1 className="text-8xl  text-center font-thin transition  duration-300 ease-in-out tracking-tight hover:tracking-wide	w-full  ">
+          Albion Vuart
+        </h1>
+      </div>
       <ul className="flex md:flex-row justify-around font-thin text-blue-500 mt-10">
         <Link href="/" className="tracking-tight hover:tracking-wide">
           <li className="text-xl">Home</li>
