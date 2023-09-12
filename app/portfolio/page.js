b924d4a0cc30cb145333e638dcda469ee14a0cdd
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-12 16:08:08 -0300

    add style like homepage

--- app/portfolio/page.js
+++ app/portfolio/page.js
@@ -1,12 +1,57 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Portfolio() {
   return (
-    <div>
-      <h1 className="text-white font-light ">Portfolio</h1>
-      <Link href="/">
-        <h2>Back</h2>
-      </Link>
-    </div>
+    <main className="flex-col px-24 pt-20 bg-gray-100	">
+      <div id="main">
+        <div>
+          <Navbar />
+        </div>
+        <div>
+          <Image
+            src="/IMG_7248.jpg"
+            alt="image of landscape"
+            width={300}
+            height={300}
+          />
+          <Image
+            src="/IMG_7249.jpg"
+            alt="image of landscape"
+            width={300}
+            height={300}
+          />
+          <Image
+            src="/IMG_7247.jpg"
+            alt="image of portrait"
+            width={300}
+            height={300}
+          />
+        </div>
+      </div>
+      <Footer />
+    </main>
   );
 }
+/*
+<Image
+          src="/IMG_7248.jpg"
+          alt="image of landscape"
+          width={300}
+          height={300}
+        />
+        <Image
+          src="/IMG_7249.jpg"
+          alt="image of landscape"
+          width={300}
+          height={300}
+        />
+        <Image
+          src="/IMG_7247.jpg"
+          alt="image of portrait"
+          width={300}
+          height={300}
+        />
+*/
