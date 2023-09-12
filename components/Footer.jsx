dc90178ff7b10aa056b6a54ba5fb362b8de7b511
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-12 19:35:35 -0300

    add border top

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import SocialMedia from "./SocialMedia";
 
 export default function Footer() {
   return (
-    <footer className="bg-slate-200  md:h-10 h-50 flex flex-col md:flex-row justify-evenly md:items-center">
+    <footer className="border-solid border-t-2 border-gray-300  md:h-10 h-50 flex flex-col md:flex-row justify-evenly md:items-center">
       <section className="mb-5 md:w-1/2 flex">
         <SocialMedia />
       </section>
