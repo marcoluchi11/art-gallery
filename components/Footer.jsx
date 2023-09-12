29e9a2d212ef39a02e0f9d818ecbb9e80fcd353e
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-12 16:02:31 -0300

    add styles to look better in mobile and desktop

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,14 +2,16 @@ import SocialMedia from "./SocialMedia";
 
 export default function Footer() {
   return (
-    <footer>
-      <div>
+    <footer className="bg-slate-200  md:h-10 h-50 flex flex-col md:flex-row justify-evenly md:items-center">
+      <section className="mb-5 md:w-1/2 flex">
         <SocialMedia />
-      </div>
-      <div>
-        Copyright 2023 &copy; Albion Vuart All rights reserved . Made by Marco
-        Luchi
-      </div>
+      </section>
+      <section className="text-center text-xs md:text-sm md:w-1/2 font-thin">
+        <p>
+          Copyright 2023 &copy; Albion Vuart All rights reserved - Made by Marco
+          Luchi
+        </p>
+      </section>
     </footer>
   );
 }
--- components/SocialMedia.jsx
+++ components/SocialMedia.jsx
@@ -1,11 +1,18 @@
+import Link from "next/link";
 import { FaInstagram, FaRegEnvelope, FaLinkedinIn } from "react-icons/fa6";
 const SocialMedia = () => {
   return (
-    <div>
-      <FaInstagram className="text-4xl text-white" />
-      <FaLinkedinIn className="text-4xl text-white" />
-      <FaRegEnvelope className="text-4xl text-white" />
-    </div>
+    <section className="flex items-center justify-around pt-5 md:pt-0 w-full md:mt-5">
+      <Link href="https://www.linkedin.com" target="_blank">
+        <FaLinkedinIn className="text-2xl" />
+      </Link>
+      <Link href="https://www.instagram.com" target="_blank">
+        <FaInstagram className="text-2xl" />
+      </Link>
+      <Link href="https://www.outlook.com" target="_blank">
+        <FaRegEnvelope className=" text-2xl" />
+      </Link>
+    </section>
   );
 };
 
