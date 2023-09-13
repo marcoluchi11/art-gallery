e97a34f1f100e38290dcdbac3a4c01491e369bce
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-13 20:58:27 -0300

    add obsession collection

---
+++ app/artwork/obsession/page.js
@@ -0,0 +1,9 @@
+import Obsesion from "@/components/Obsesion";
+
+export default function Obsession() {
+  return (
+    <main className="flex-col md:px-24 pt-20 bg-gray-100">
+      <Obsesion />
+    </main>
+  );
+}
