262fade3aa125ad52171fd47f4a10be9aa06f584
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-13 20:58:58 -0300

    add component for images

---
+++ components/Obsesion.jsx
@@ -0,0 +1,158 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Obsesion() {
+  return (
+    <>
+      <Link
+        href="mailto:albionvu.art@gmail.com"
+        target="_blank"
+        className=" flex justify-center items-center mt-[150px] underline"
+      >
+        Click here to inquire about available work and pricing.
+      </Link>
+      <div className="flex flex-row flex-wrap justify-center md:flex-row md:flex-wrap mt-[80px] ">
+        <Image
+          className="mr-2"
+          src="/obsession/IMG-7254.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7255.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7256.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7257.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7258.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7259.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7260.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7261.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7263.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7264.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7265.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7266.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7267.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7269.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7270.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7271.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7272.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7274.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7278.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+        <Image
+          className="mr-2 mb-2"
+          src="/obsession/IMG-7279.jpg"
+          alt="painting"
+          width={200}
+          height={300}
+        />
+      </div>
+    </>
+  );
+}
