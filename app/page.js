4a2cc8e1edc39aeaea4c481f39e7a5448dde45f7
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-13 21:04:21 -0300

    delete unnecesary lines

--- app/page.js
+++ app/page.js
@@ -1,17 +1,9 @@
 import Carusel from "@/components/Carusel";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex-col md:px-24 pt-20 bg-gray-100	">
-      <div id="main">
-        <div>
-          <Navbar />
-        </div>
-        <Carusel />
-      </div>
-      <Footer />
+    <main className="flex-col mt-20 md:px-24 pt-20 bg-gray-100">
+      <Carusel />
     </main>
   );
 }
