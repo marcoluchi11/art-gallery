5fb93a53f14a644acbbbd1bdc55948dfe2f49c3f
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-15 18:26:23 -0300

    change text to white majorly

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,25 +1,27 @@
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
-import Link from "next/link";
+import NewsletterForm from "@/components/NewsletterForm";
 import { FaPhone, FaEnvelope } from "react-icons/fa6";
 export default function Contact() {
   return (
-    <main className="flex-col md:px-24 pt-20 bg-gray-100">
-      <div className="flex flex-col my-10 text-center mt-[150px]">
-        <h1 className=" text-3xl mt-10 md:text-5xl font-thin tracking-tight">
+    <main className="flex-col md:px-24 pt-20 mt-20 md:pt-0 md:mt-0 ">
+      <div className="flex flex-col  text-center mt-[150px]">
+        <h1 className=" text-3xl mt-10 md:text-5xl font-thin tracking-tight text-white">
           GET IN TOUCH WITH ME
         </h1>
-        <p className="my-10 font-light md:w-full md:mx-0 mx-3	">
+        <p className="my-10 font-light md:w-full md:mx-0 mx-3	 text-white">
           Got a question about us? Are you interested in partnering with us?
           Have some suggestions or just want to say Hi? Just contact us.
         </p>
         <div className="flex justify-center">
-          <FaPhone className="text-3xl mx-3" />
-          <p className="underline cursor-pointer">+1 (917) 3028958</p>
+          <FaPhone className="text-3xl mx-3" color="white" fill="white" />
+          <p className="underline cursor-pointer text-white">
+            +1 (917) 3028958
+          </p>
         </div>
         <div className="flex justify-center mt-5">
-          <FaEnvelope className="text-3xl mx-3" />
-          <p className="underline cursor-pointer">albionvu.art@gmail.com</p>
+          <FaEnvelope className="text-3xl mx-3" color="white" fill="white" />
+          <p className="underline cursor-pointer text-white">
+            albionvu.art@gmail.com
+          </p>
         </div>
       </div>
     </main>
