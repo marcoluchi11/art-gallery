41df702cc5a2bd4fe1685adbfa2ea462c7168474
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-15 18:20:30 -0300

    add underconstruction page

--- app/sketches/page.js
+++ app/sketches/page.js
@@ -3,8 +3,8 @@ import Navbar from "@/components/Navbar";
 
 export default function Sketches() {
   return (
-    <main className="flex-col md:px-24 pt-20 bg-gray-100">
-      <article>Under Construction...</article>
+    <main className="flex-col md:px-24 pt-20 ">
+      <article className="text-white">Under Construction...</article>
     </main>
   );
 }
