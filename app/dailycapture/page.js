ca8b53dc286fe4eded777beadea496fd57b0f8ba
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-15 18:24:02 -0300

    add dailycapture instead of photography

---
+++ app/dailycapture/page.js
@@ -0,0 +1,7 @@
+export default function Photography() {
+  return (
+    <main className="flex-col md:px-24 pt-20 ">
+      <h1 className="mt-[50px] text-white">Under Construction...</h1>
+    </main>
+  );
+}
