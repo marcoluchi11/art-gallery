fea889f44e0986d0a6a91ad0cce7a7f4fd8574bb
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-15 14:48:10 -0300

    delete everything for later

--- app/artwork/page.js
+++ app/artwork/page.js
@@ -1,36 +1,9 @@
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Artwork() {
   return (
-    <main className="flex-col md:px-24 pt-20 bg-gray-100	">
-      <div id="main">
-        <div>
-          <Navbar />
-        </div>
-        <div className="flex flex-col items-center lg:flex-row my-10 ">
-          <Image
-            src="/IMG_7248.jpg"
-            alt="image of landscape"
-            width={300}
-            height={300}
-          />
-          <Image
-            src="/IMG_7249.jpg"
-            alt="image of landscape"
-            width={300}
-            height={300}
-          />
-          <Image
-            src="/IMG_7247.jpg"
-            alt="image of portrait"
-            width={300}
-            height={300}
-          />
-        </div>
-      </div>
-      <Footer />
+    <main className="flex-col md:px-24 pt-20 bg-gray-100">
+      <div className="flex flex-col items-center lg:flex-row my-10 "></div>
     </main>
   );
 }
