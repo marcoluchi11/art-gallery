d5436ecf746ec8d1b390f5a917b2eb0fdf4fda25
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-15 21:51:05 -0300

    add under curaton sign

--- app/sketches/page.js
+++ app/sketches/page.js
@@ -3,8 +3,15 @@ import Navbar from "@/components/Navbar";
 
 export default function Sketches() {
   return (
-    <main className="flex-col md:px-24 pt-20 ">
-      <article className="text-white">Under Construction...</article>
+    <main className="flex-col md:px-24 pt-20 mt-20 ">
+      <article className="text-white">
+        <h1 className=" flex flex-col text-4xl text-center text-gray-200 uppercase">
+          Under Curation...
+        </h1>
+        <h1 className="flex flex-col text-2xl text-center text-gray-200 uppercase">
+          More to come
+        </h1>
+      </article>
     </main>
   );
 }
