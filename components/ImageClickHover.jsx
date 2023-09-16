b319d1bdebf4409238a54764f97e8ddcbdb54d4a
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-15 21:49:52 -0300

    add change to show texxt when get into small devices instead of hover

--- components/ImageClickHover.jsx
+++ components/ImageClickHover.jsx
@@ -5,7 +5,7 @@ import { useContext, useState } from "react";
 import { ArtContext } from "@/context/context";
 
 export default function ImageClickHover({
-  text,
+  text = "",
   source,
   sizeW,
   sizeH,
@@ -17,13 +17,13 @@ export default function ImageClickHover({
     setShowModal(!showModal);
   };
   return (
-    <div className="relative cursor-pointer">
+    <div className="relative cursor-pointer flex flex-col mx-auto">
       <Image
         quality={100}
         // onClick={() => handleClick()}
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => setHovered(false)}
-        className={`mb-[200px] ${margin ? "mr-3" : ""} ${
+        className={`md:mb-[200px] ${margin ? "mr-3" : ""} ${
           hovered
             ? "transition-opacity opacity-40"
             : " transition-opacity bg-opacity-100"
@@ -33,10 +33,13 @@ export default function ImageClickHover({
         width={sizeW || 700}
         height={sizeH || 500}
       />
+      <p className="flex md:hidden font-light uppercase text-black mb-[200px] justify-center">
+        {text}
+      </p>
       {hovered && (
         <p
           onMouseEnter={() => setHovered(true)}
-          className="mensaje bg-gray-100 rounded-md absolute top-[50%] left-[50%] text-black font-light text-xl text-center opacity-100 flex flex-col"
+          className="hidden md:flex mensaje bg-gray-100 rounded-md absolute top-[50%] left-[50%] text-black font-light text-xl text-center opacity-100 flex-col"
         >
           {text}
           <Inquire />
