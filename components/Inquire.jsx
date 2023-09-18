89731e4758914e7d20075188d682cbb5ad987d63
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-18 18:43:05 -0300

    add _blank in anchor tag

--- components/Inquire.jsx
+++ components/Inquire.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Inquire() {
   return (
     <Link
-      target="_top"
+      target="_blank"
       className="hover:underline"
       href="mailto:albionvu.art@gmail.com"
     >
