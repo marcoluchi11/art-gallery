97653f2cc95f5e14d5182ef30b66e5e3d88b4245
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-19 20:48:23 -0300

    delete daily capture because its no use

--- app/dailycapture/page.js
+++
@@ -1,9 +0,0 @@
-import DailyCapture from "@/components/DailyCapture";
-
-export default function Photography() {
-  return (
-    <main className="flex-col md:px-24 pt-20 bg-gray-100 ">
-      <DailyCapture />
-    </main>
-  );
-}
