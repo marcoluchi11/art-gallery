edf57747e1e9dd02437272253483f24396c6ba5b
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-20 22:34:38 -0300

    delete itsatouch fcollection

--- app/artwork/itsatouch/page.js
+++
@@ -1,9 +0,0 @@
-import Itsatouch from "@/components/Itsatouch";
-
-export default function Info() {
-  return (
-    <main className="flex-col md:px-24 pt-20 bg-gray-100">
-      <Itsatouch />
-    </main>
-  );
-}
