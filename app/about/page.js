83e8ffe59aea6d96670f1bb8c5a350c91a248d30
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-21 08:59:58 -0300

    comment on newsletter form

--- app/about/page.js
+++ app/about/page.js
@@ -1,9 +1,11 @@
 import Bio from "@/components/Bio";
+// import NewsletterForm from "@/components/NewsletterForm";
 
 export default function About() {
   return (
     <main className="flex-col md:px-24 pt-5 ">
       <Bio />
+      {/* <NewsletterForm /> */}
     </main>
   );
 }
