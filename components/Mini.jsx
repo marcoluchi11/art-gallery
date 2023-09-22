2e9b2992e2f8f221a13656b432526962857f3fa0
marcoluchi11 <marcoaluchi@hotmail.com>
2023-09-21 21:28:38 -0300

    delete duplicate images and reduce images in size

--- components/Mini.jsx
+++ components/Mini.jsx
@@ -11,89 +11,123 @@ export default function Mini() {
         }  `}
       >
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141039.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141124.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141159.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141231.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141256.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141327.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141352.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141419.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141450.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141517.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141539.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141604.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141632.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141650.jpg"
         />
         <div className="flex">
           <ImageClickHover
+            sizeH={500}
+            sizeW={500}
             gap={true}
             text="ATHENES 11x15
 Acrylics on paper"
             source="/mini/141711.jpg"
           />
           <ImageClickHover
+            sizeH={500}
+            sizeW={500}
             gap={true}
             text="ATHENES 11x15
 Acrylics on paper"
             source="/mini/141738.jpg"
           />
           <ImageClickHover
+            sizeH={500}
+            sizeW={500}
             gap={true}
             text="ATHENES 11x15
 Acrylics on paper"
@@ -101,44 +135,60 @@ Acrylics on paper"
           />
         </div>
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141839.jpg"
         />
         <ImageClickHover
-          text="ATHENES 11x15
-Acrylics on paper"
-          source="/mini/141904.jpg"
-        />
-        <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141926.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
+          text="ATHENES 11x15
+Acrylics on paper"
+          source="/mini/141904.jpg"
+        />
+        <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/141950.jpg"
         />
         <ImageClickHover
+          sizeH={500}
+          sizeW={500}
           text="ATHENES 11x15
 Acrylics on paper"
           source="/mini/142034.jpg"
         />
         <div className="flex">
           <ImageClickHover
+            sizeH={500}
+            sizeW={500}
             gap={true}
             text="ATHENES 11x15
 Acrylics on paper"
             source="/mini/142319.jpg"
           />
           <ImageClickHover
+            sizeH={500}
+            sizeW={500}
             gap={true}
             text="ATHENES 11x15
 Acrylics on paper"
             source="/mini/142335.jpg"
           />
           <ImageClickHover
+            sizeH={500}
+            sizeW={500}
             gap={true}
             text="ATHENES 11x15
 Acrylics on paper"
@@ -146,11 +196,6 @@ Acrylics on paper"
           />
         </div>
 
-        <ImageClickHover
-          text="ATHENES 11x15
-Acrylics on paper"
-          source="/mini/141904.jpg"
-        />
         <h1 className="uppercase text-center text-3xl my-3">
           prints available
         </h1>
