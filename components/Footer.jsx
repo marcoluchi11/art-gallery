344261d94921a845838aefbc3b274e8849cfde3b
marcoluchi11 <marcoaluchi@hotmail.com>
2023-10-17 17:37:30 -0300

    add new image to homepage

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import SocialMedia from "./SocialMedia";
 
 export default function Footer() {
@@ -6,11 +7,18 @@ export default function Footer() {
       <section className="mb-5 md:w-1/2 flex">
         <SocialMedia />
       </section>
-      <section className="text-center text-xs md:text-sm md:w-1/2 font-light">
+      <section className="text-center flex flex-col md:flex-row items-center justify-around text-xs md:text-sm md:w-1/2 font-light">
         <p className="text-white">
           Copyright &copy; Albion Vu - All rights reserved - All the work shown
           on the website is the originals
         </p>
+        <Image
+          className="mt-5 mb-3 md:mt-0 md:mb-0"
+          src="/frame.png"
+          alt="qrcode"
+          width={35}
+          height={25}
+        />
       </section>
     </footer>
   );
--- components/HomeImages.jsx
+++ components/HomeImages.jsx
@@ -20,6 +20,13 @@ export default function HomeImages() {
 
       <Image
         src="/background3.jpg"
+        className="w-[600px] mb-10 md:w-[1000px]"
+        alt="homepage painting"
+        width={800}
+        height={800}
+      />
+      <Image
+        src="/homepageimg.jpg"
         className="w-[600px] mb-[200px] md:mb-[250px] md:w-[1000px]"
         alt="homepage painting"
         width={800}
