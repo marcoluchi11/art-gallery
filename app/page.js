33c9b0d837ff6b4778897b2c89ee2ee0acbc57cf
marcoluchi11 <marcoaluchi@hotmail.com>
2023-10-31 12:23:58 -0300

    reduce margin top

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Homepage from "@/components/HomePage";
 
 export default function Home() {
   return (
-    <main className="flex flex-col mt-[300px] md:mt-10 md:px-24 ">
+    <main className="flex flex-col mt-[135px] md:mt-10 md:px-24 ">
       <Homepage />
     </main>
   );
