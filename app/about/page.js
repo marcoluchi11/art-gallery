207f9aada13fa5cf91d636cb959029cdaaf35165
marcoluchi11 <marcoaluchi@hotmail.com>
2023-11-02 21:05:07 -0300

    delete comments

--- app/about/page.js
+++ app/about/page.js
@@ -1,11 +1,9 @@
 import Bio from "@/components/Bio";
-// import NewsletterForm from "@/components/NewsletterForm";
 
 export default function About() {
   return (
     <main className="flex-col md:px-24 pt-5 ">
       <Bio />
-      {/* <NewsletterForm /> */}
     </main>
   );
 }
