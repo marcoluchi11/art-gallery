a7f243549257ab5fac10f170f3a1f7a652f8942c
marcoluchi11 <marcoaluchi@hotmail.com>
2023-12-12 19:08:39 -0300

    make the images smaller, but when you click it grows 700 width

--- components/ImageClickHover.jsx
+++ components/ImageClickHover.jsx
@@ -1,8 +1,6 @@
 "use client";
 import Image from "next/image";
-import Inquire from "./Inquire";
-import { useContext, useState } from "react";
-import { ArtContext } from "@/context/context";
+import { useState } from "react";
 export default function ImageClickHover({
   text = "",
   source,
@@ -12,38 +10,39 @@ export default function ImageClickHover({
   sold = false,
   gap = false,
 }) {
-  const { setShowModal, showModal } = useContext(ArtContext);
+  const [isEnlarged, setIsEnlarged] = useState(false);
+
+  const toggleEnlarged = () => {
+    setIsEnlarged(!isEnlarged);
+  };
   const [hovered, setHovered] = useState(false);
   return (
     <div className="relative cursor-pointer flex flex-col mx-auto">
       <Image
         quality={100}
-        // onClick={() => handleClick()}
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        className={`] ${margin ? "mr-3" : ""} ${
-          hovered
-            ? "transition-all duration-500 ease-in-out opacity-40"
-            : "transition-all duration-500 ease-in-out opacity-100"
-        } ${sold ? "" : "md:mb-[200px]"}  ${gap ? "mr-3" : ""}`}
+        onClick={toggleEnlarged}
+        // onMouseEnter={() => setIsEnlarged(true)}
+        // onMouseLeave={() => setIsEnlarged(false)}
+        className={` ease-in-out transition-width duration-500 cursor-pointer rounded-sm ${
+          isEnlarged ? "" : ""
+        }
+         mr-3 `}
         src={source}
         alt="painting"
-        width={sizeW || 700}
+        width={isEnlarged ? 700 : 100}
         height={sizeH || 500}
       />
-      <p
+      {/* <p
         className={`flex md:hidden font-light ${
-          sold ? "" : "mb-[200px]"
+          sold ? "" : ""
         } uppercase text-black justify-center text-center`}
       >
         {text}
-      </p>
+      </p> */}
       {sold && (
-        <p className="text-center font-light mb-[200px] text-red-500 text-3xl">
-          SOLD
-        </p>
+        <p className="text-center font-light text-red-500 text-xl">SOLD</p>
       )}
-      {hovered && (
+      {/* {hovered && (
         <p
           onMouseEnter={() => setHovered(true)}
           className="hidden md:flex mensaje bg-gray-100 rounded-md absolute top-[50%] left-[50%] text-black font-light text-xl text-center opacity-100 flex-col"
@@ -51,7 +50,7 @@ export default function ImageClickHover({
           {text}
           <Inquire />
         </p>
-      )}
+      )} */}
     </div>
   );
 }
