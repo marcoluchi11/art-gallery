6c83a6af229c5bc88afca23d8550ac83312d4e73
marcoluchi11 <marcoaluchi@hotmail.com>
2023-12-14 13:48:37 -0300

    add text-center

--- components/InquirePrints.jsx
+++ components/InquirePrints.jsx
@@ -9,11 +9,11 @@ export default function InquirePrints() {
       <Link
         href="mailto:albionvu.art@gmail.com?subject=Inquiry%20About%20Pricing&body=Hello%20there%2C%0A%0AI%20am%20interested%20in%20inquiring%20about%20the%20pricing%20of%20a%20specific%20piece%20and%20would%20like%20to%20receive%20a%20full%20catalogue.%0A%0AThank%20you.%0A%0A"
         target="-blank"
-        className=" flex justify-center items-center mt-10 uppercase font-light  "
+        className=" flex justify-center text-center items-center mt-10 uppercase font-light  "
       >
         Click here to inquire about available work and pricing.
       </Link>
-      <small className="flex justify-center mt-10 uppercase text-xs">
+      <small className="flex justify-center text-center mt-10 uppercase text-xs">
         CLICK THE IMAGES TO SEE THE ART PIECES ON A LARGER FORMAT
       </small>
     </>
