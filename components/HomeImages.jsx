c3c9ee2de3c419082a86e105c9d0dc243e87f0e9
marcoluchi11 <marcoaluchi@hotmail.com>
2023-12-13 21:11:09 -0300

    add patches collection to homepage

--- components/HomeImages.jsx
+++ components/HomeImages.jsx
@@ -6,10 +6,17 @@ import ImageClickHover from "./ImageClickHover";
 export default function HomeImages() {
   return (
     <div className="flex flex-wrap mt-[80px] ">
-      <div className="flex flex-col md:flex-row mb-20 items-center gap-10 md:gap-0 w-full mt-10">
+      <div className="flex flex-wrap md:flex-row mb-20 items-center gap-10 md:gap-0 w-full mt-10">
         <ImageClickHover source="/homeimage1.jpg" sizeW={200} />
         <ImageClickHover source="/homeimage2.jpg" sizeW={200} />
         <ImageClickHover source="/homeimage3.jpg" sizeW={200} />
+      </div>
+      <div className="flex flex-wrap md:flex-row mb-20 items-center gap-10 md:gap-0 w-full mt-10">
+        <ImageClickHover source="/patches/patches1.jpg" sizeW={200} />
+        <ImageClickHover source="/patches/patches2.jpg" sizeW={200} />
+        <ImageClickHover source="/patches/patches3.jpg" sizeW={200} />
+        <ImageClickHover source="/patches/patches4.jpg" sizeW={200} />
+        <ImageClickHover source="/patches/patches5.jpg" sizeW={200} />
       </div>
     </div>
   );
