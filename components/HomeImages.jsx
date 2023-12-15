2e6d6ac924663770ca2d514b0c5497bff7cd0f67
marcoluchi11 <marcoaluchi@hotmail.com>
2023-12-15 01:19:36 -0300

    add bigger size to images in homepage

--- components/HomeImages.jsx
+++ components/HomeImages.jsx
@@ -7,16 +7,16 @@ export default function HomeImages() {
   return (
     <div className="flex flex-wrap mt-[80px] ">
       <div className="flex flex-wrap  mb-20 items-center gap-10 md:gap-0 w-full mt-10">
-        <ImageClickHover source="/homeimage1.jpg" sizeW={100} />
-        <ImageClickHover source="/homeimage2.jpg" sizeW={100} />
-        <ImageClickHover source="/homeimage3.jpg" sizeW={100} />
+        <ImageClickHover source="/homeimage1.jpg" sizeW={200} />
+        <ImageClickHover source="/homeimage2.jpg" sizeW={200} />
+        <ImageClickHover source="/homeimage3.jpg" sizeW={200} />
       </div>
       <div className="flex flex-wrap mb-20 items-center gap-10 md:gap-0 w-full ">
-        <ImageClickHover source="/patches/patches1.jpg" sizeW={100} />
-        <ImageClickHover source="/patches/patches2.jpg" sizeW={100} />
-        <ImageClickHover source="/patches/patches3.jpg" sizeW={100} />
-        <ImageClickHover source="/patches/patches4.jpg" sizeW={100} />
-        <ImageClickHover source="/patches/patches5.jpg" sizeW={100} />
+        <ImageClickHover source="/patches/patches1.jpg" sizeW={175} />
+        <ImageClickHover source="/patches/patches2.jpg" sizeW={175} />
+        <ImageClickHover source="/patches/patches3.jpg" sizeW={175} />
+        <ImageClickHover source="/patches/patches4.jpg" sizeW={175} />
+        <ImageClickHover source="/patches/patches5.jpg" sizeW={175} />
       </div>
     </div>
   );
