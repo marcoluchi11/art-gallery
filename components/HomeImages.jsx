2a9e7c46b4a4ab080cb2c29bcc0d69986eca15b9
marcoluchi11 <marcoaluchi@hotmail.com>
2023-12-24 17:56:08 -0300

    add homepages images to patches and add new ones there

--- components/HomeImages.jsx
+++ components/HomeImages.jsx
@@ -1,6 +1,4 @@
 "use client";
-import Image from "next/image";
-import { useState } from "react";
 import ImageClickHover from "./ImageClickHover";
 
 export default function HomeImages() {
@@ -9,19 +7,15 @@ export default function HomeImages() {
       <small className="flex justify-center text-center mt-10 uppercase text-xs text-white">
         CLICK THE IMAGES TO SEE THE ART PIECES ON A LARGER FORMAT
       </small>
-      <div className="flex flex-wrap mt-[80px] ">
-        <div className="flex flex-wrap  mb-20 items-center gap-10 md:gap-0 w-full mt-10">
-          <ImageClickHover source="/homeimage1.jpg" sizeW={200} />
-          <ImageClickHover source="/homeimage2.jpg" sizeW={200} />
-          <ImageClickHover source="/homeimage3.jpg" sizeW={200} />
-        </div>
-        <div className="flex flex-wrap mb-20 items-center gap-10 md:gap-0 w-full ">
-          <ImageClickHover source="/patches/patches1.jpg" sizeW={175} />
-          <ImageClickHover source="/patches/patches2.jpg" sizeW={175} />
-          <ImageClickHover source="/patches/patches3.jpg" sizeW={175} />
-          <ImageClickHover source="/patches/patches4.jpg" sizeW={175} />
-          <ImageClickHover source="/patches/patches5.jpg" sizeW={175} />
-        </div>
+      <div className="flex flex-wrap mt-[80px] mb-20">
+        <ImageClickHover padding={true} source="/homepage1.jpg" sizeW={220} />
+        <ImageClickHover padding={true} source="/homepage2.jpg" sizeW={220} />
+        <ImageClickHover padding={true} source="/homepage3.jpg" sizeW={220} />
+      </div>
+      <div className="flex flex-wrap ">
+        <ImageClickHover padding={true} source="/homepage4.jpg" sizeW={220} />
+        <ImageClickHover padding={true} source="/homepage5.jpg" sizeW={220} />
+        <ImageClickHover padding={true} source="/homepage6.jpg" sizeW={220} />
       </div>
     </>
   );
