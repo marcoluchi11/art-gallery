59c3bb7baed06eb31580a23fc18d43fb3c02fb18
marcoluchi11 <marcoaluchi@hotmail.com>
2023-12-28 18:05:14 -0300

    add margin left to links

--- components/Press.jsx
+++ components/Press.jsx
@@ -4,103 +4,103 @@ import PressArticle from "./PressArticle";
 
 export default function Press() {
   return (
-    <section className="flex justify-center items-center mt-[100px] md:mt-0 flex-col md:flex-row my-10 flex-wrap">
+    <section className="flex justify-around items-center mt-[50px] md:mt-0 flex-col md:flex-row my-10 flex-wrap">
       <Image
         src="/press/IMG-7427.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7428.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7429.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7430.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7431.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7432.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7433.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7434.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7435.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7436.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7437.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7438.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7439.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
       <Image
         src="/press/IMG-7440.jpg"
         alt="Press"
-        width={350}
-        height={350}
+        width={280}
+        height={280}
         className="my-10 md:mr-10"
       />
     </section>
