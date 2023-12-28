19c6f35ce615d7d020d5e4161e326afbc0e6c40a
marcoluchi11 <marcoaluchi@hotmail.com>
2023-12-28 16:39:09 -0300

    add artcile component in press route

--- app/press/page.js
+++ app/press/page.js
@@ -1,8 +1,10 @@
 import Press from "@/components/Press";
+import PressArticle from "@/components/PressArticle";
 
 export default function PressInfo() {
   return (
     <main className="flex-col md:px-24 pt-5 ">
+      <PressArticle />
       <Press />
     </main>
   );
