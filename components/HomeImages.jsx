12e2b9433fea15a55fd30a5bead4144f52e06a4f
marcoluchi11 <marcoaluchi@hotmail.com>
2024-01-01 20:13:05 -0300

    add text to component in homeimages

--- components/HomeImages.jsx
+++ components/HomeImages.jsx
@@ -15,12 +15,14 @@ export default function HomeImages() {
         />
         <ImageClickHover
           padding={true}
+          text="LINE PATH 56x38’ ACRYLICS AND OIL PASTEL OVER CANVAS"
           source="/linepath/linepath2.jpg"
           sizeW={180}
         />
         <ImageClickHover
           padding={true}
           source="/linepath/linepath3.jpg"
+          text="LINE PATH 58X43’ ACRYLICS AND OIL PASTEL OVER CANVAS"
           sizeW={180}
         />
       </div>
@@ -29,15 +31,18 @@ export default function HomeImages() {
           padding={true}
           source="/linepath/linepath4.jpg"
           sizeW={180}
+          text="LINE PATH 80X60’ ACRYLICS AND OIL PASTEL OVER CANVAS"
         />
         <ImageClickHover
           padding={true}
           source="/linepath/linepath5.jpg"
           sizeW={180}
+          text="LINE PATH 42X57’ ACRYLICS AND OIL PASTEL OVER CANVAS"
         />
         <ImageClickHover
           padding={true}
           source="/linepath/linepath6.jpg"
+          text="LINE PATH 56X56’ ACRYLICS AND OIL PASTEL OVER CANVAS"
           sizeW={180}
         />
       </div>
@@ -46,11 +51,13 @@ export default function HomeImages() {
           padding={true}
           source="/linepath/linepath7.jpg"
           sizeW={180}
+          text="LINE PATH 54X38’ ACRYLICS AND OIL PASTEL OVER CANVAS"
         />
         <ImageClickHover
           padding={true}
           source="/linepath/linepath8.jpg"
           sizeW={180}
+          text="LINE PATH 55X34’ ACRYLICS AND OIL PASTEL OVER CANVAS"
         />
       </div>
     </>
