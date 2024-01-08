adec7426db6e328d269f5a411d6026bf15f687e1
marcoluchi11 <marcoaluchi@hotmail.com>
2024-01-08 19:19:52 -0300

    add analytics from Vercel

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,10 @@
-import Navbar from "@/components/Navbar";
 import Navbar2 from "@/components/Navbar2";
 import "./globals.css";
 import { Roboto } from "next/font/google";
 import Footer from "@/components/contact/Footer";
 import { ArtProvider } from "@/context/context";
 import BackToTop from "@/components/BackToTop";
-
+import { Analytics } from "@vercel/analytics/react";
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["100", "300", "500", "700", "900"],
@@ -23,6 +22,7 @@ export default function RootLayout({ children }) {
         <ArtProvider>
           <Navbar2 />
           {children}
+          <Analytics />
           <BackToTop />
           <Footer />
         </ArtProvider>
