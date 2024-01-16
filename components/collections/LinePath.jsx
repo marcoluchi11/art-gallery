cba88034f16dd481332f085ba6a1df66d3a24804
marcoluchi11 <marcoaluchi@hotmail.com>
2024-01-16 17:39:24 -0300

    change order of paintings for color lookalike

--- components/collections/LinePath.jsx
+++ components/collections/LinePath.jsx
@@ -26,49 +26,55 @@ export default function LinePath() {
       <InquirePrints />
       <div className="flex flex-wrap mt-[80px] ">
         <div className="flex flex-wrap md:flex-row gap-5 md:gap-10  w-full mt-10 mb-10">
-          <ImageClickHover source="/linepath/linepath1.jpg" />
-          <ImageClickHover
-            source="/linepath/linepath2.jpg"
-            text="LINE PATH 56x38’ ACRYLICS AND OIL PASTEL OVER CANVAS"
-          />
-          <ImageClickHover
-            source="/linepath/linepath3.jpg"
-            text="LINE PATH 58X43’ ACRYLICS AND OIL PASTEL OVER CANVAS"
-          />
-          <ImageClickHover
-            source="/linepath/linepath4.jpg"
-            text="LINE PATH 80X60’ ACRYLICS AND OIL PASTEL OVER CANVAS"
-          />
+          <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
+            <ImageClickHover
+              source="/linepath/linepath4.jpg"
+              text="LINE PATH 80X60’ ACRYLICS AND OIL PASTEL OVER CANVAS"
+            />
+
+            <ImageClickHover
+              source="/linepath/linepath6.jpg"
+              text="LINE PATH 56X56’ ACRYLICS AND OIL PASTEL OVER CANVAS"
+            />
+            <ImageClickHover
+              source="/linepath/linepath10.jpg"
+              text="LINE PATH 58X58’ ACRYLICS AND OIL PASTEL OVER CANVAS"
+            />
+          </div>
+          <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
+            <ImageClickHover source="/linepath/linepath1.jpg" />
+            <ImageClickHover
+              source="/linepath/linepath8.jpg"
+              text="LINE PATH 55X34’ ACRYLICS AND OIL PASTEL OVER CANVAS"
+            />
+            <ImageClickHover
+              source="/linepath/linepath2.jpg"
+              text="LINE PATH 56x38’ ACRYLICS AND OIL PASTEL OVER CANVAS"
+            />
+          </div>
           <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
             <ImageClickHover
               source="/linepath/linepath5.jpg"
               text="LINE PATH 42X57’ ACRYLICS AND OIL PASTEL OVER CANVAS"
             />
             <ImageClickHover
-              source="/linepath/linepath6.jpg"
-              text="LINE PATH 56X56’ ACRYLICS AND OIL PASTEL OVER CANVAS"
+              source="/linepath/linepath3.jpg"
+              text="LINE PATH 58X43’ ACRYLICS AND OIL PASTEL OVER CANVAS"
             />
+
             <ImageClickHover
               source="/linepath/linepath7.jpg"
               text="LINE PATH 54X38’ ACRYLICS AND OIL PASTEL OVER CANVAS"
             />
-            <ImageClickHover
-              source="/linepath/linepath8.jpg"
-              text="LINE PATH 55X34’ ACRYLICS AND OIL PASTEL OVER CANVAS"
-            />
           </div>
           <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
             <ImageClickHover
-              source="/linepath/linepath9.jpg"
-              text="LINE PATH 56X44’ ACRYLICS AND OIL PASTEL OVER CANVAS"
-            />
-            <ImageClickHover
-              source="/linepath/linepath10.jpg"
-              text="LINE PATH 58X58’ ACRYLICS AND OIL PASTEL OVER CANVAS"
-            />
-            <ImageClickHover
               source="/linepath/linepath11.jpg"
               text="LINE PATH 43X54’ ACRYLICS AND OIL PASTEL OVER CANVAS"
+            />
+            <ImageClickHover
+              source="/linepath/linepath9.jpg"
+              text="LINE PATH 56X44’ ACRYLICS AND OIL PASTEL OVER CANVAS"
             />
           </div>
         </div>
