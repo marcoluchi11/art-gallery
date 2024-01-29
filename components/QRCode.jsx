46d021416f9d6a140a7f92a34e9645050d1b352a
marcoluchi11 <marcoaluchi@hotmail.com>
2024-01-29 16:07:17 -0300

    delete path

--- components/QRCode.jsx
+++
@@ -1,9 +0,0 @@
-import { QRCodeSVG } from "qrcode.react";
-
-export default function QRCode() {
-  return (
-    <div className="my-3 md:my-0">
-      <QRCodeSVG value="https://www.albionvuart.com/" size={55} />
-    </div>
-  );
-}
