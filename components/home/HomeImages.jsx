81a2b82f733da6a96ce46a61bcb7cdde59b1919c
marcoluchi11 <marcoaluchi@hotmail.com>
2024-01-29 16:17:04 -0300

    biggser size 250

--- components/home/HomeImages.jsx
+++ components/home/HomeImages.jsx
@@ -11,59 +11,59 @@ export default function HomeImages() {
         <ImageClickHover
           padding={true}
           source="/linepath/linepath1.jpg"
-          sizeW={200}
+          sizeW={250}
         />
         <ImageClickHover
           padding={true}
           text="LINE PATH 56x38’ ACRYLICS AND OIL PASTEL OVER CANVAS"
           source="/linepath/linepath2.jpg"
-          sizeW={200}
+          sizeW={250}
         />
         <ImageClickHover
           padding={true}
           source="/linepath/linepath3.jpg"
           text="LINE PATH 58X43’ ACRYLICS AND OIL PASTEL OVER CANVAS"
-          sizeW={200}
+          sizeW={250}
         />
       </div>
       <div className="flex flex-wrap mb-20">
         <ImageClickHover
           padding={true}
           source="/linepath/linepath4.jpg"
-          sizeW={200}
+          sizeW={250}
           text="LINE PATH 80X60’ ACRYLICS AND OIL PASTEL OVER CANVAS"
         />
         <ImageClickHover
           padding={true}
           source="/linepath/linepath10.jpg"
-          sizeW={200}
+          sizeW={250}
           text="LINE PATH 58X58’ ACRYLICS AND OIL PASTEL OVER CANVAS"
         />
         <ImageClickHover
           padding={true}
           source="/linepath/linepath6.jpg"
           text="LINE PATH 56X56’ ACRYLICS AND OIL PASTEL OVER CANVAS"
-          sizeW={200}
+          sizeW={250}
         />
       </div>
       <div className="flex flex-wrap ">
         <ImageClickHover
           padding={true}
           source="/linepath/linepath7.jpg"
-          sizeW={200}
+          sizeW={250}
           text="LINE PATH 54X38’ ACRYLICS AND OIL PASTEL OVER CANVAS"
         />
         <ImageClickHover
           padding={true}
           source="/linepath/linepath8.jpg"
-          sizeW={200}
+          sizeW={250}
           text="LINE PATH 55X34’ ACRYLICS AND OIL PASTEL OVER CANVAS"
         />
 
         <ImageClickHover
           padding={true}
           source="/linepath/linepath5.jpg"
-          sizeW={200}
+          sizeW={250}
           text="LINE PATH 42X57’ ACRYLICS AND OIL PASTEL OVER CANVAS"
         />
       </div>
@@ -71,13 +71,13 @@ export default function HomeImages() {
         <ImageClickHover
           padding={true}
           source="/linepath/linepath9.jpg"
-          sizeW={200}
+          sizeW={250}
           text="LINE PATH 56X44’ ACRYLICS AND OIL PASTEL OVER CANVAS"
         />
         <ImageClickHover
           padding={true}
           source="/linepath/linepath11.jpg"
-          sizeW={200}
+          sizeW={250}
           text="LINE PATH 43X54’ ACRYLICS AND OIL PASTEL OVER CANVAS"
         />
       </div>
