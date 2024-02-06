96b01f7e87226c3ec55cfa56cff71095580dd0b1
marcoluchi11 <marcoaluchi@hotmail.com>
2024-02-06 15:59:37 -0300

    add items-center

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Homepage from "@/components/home/HomePage";
 
 export default function Home() {
   return (
-    <main className="flex flex-col mt-[135px] md:mt-10 md:px-24 ">
+    <main className="flex flex-col mt-[135px] md:mt-10 items-center ">
       <Homepage />
     </main>
   );
