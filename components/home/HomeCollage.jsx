498c7e528ba4e31534d4bffa07b0b97815df451e
marcoluchi11 <marcoaluchi@hotmail.com>
2024-02-06 15:55:59 -0300

    add homecollage component to home folder

---
+++ components/home/HomeCollage.jsx
@@ -0,0 +1,481 @@
+import ImageClickHover from "../ImageClickHover";
+
+export default function HomeCollage() {
+  return (
+    <>
+      <small className="flex justify-center text-center mt-2 uppercase text-xs font-light ">
+        CLICK THE IMAGES TO SEE THE ART PIECES ON A LARGER FORMAT
+      </small>
+      <div className="flex ">
+        {/* COLUMN 1 */}
+        <div className="flex flex-col  mt-[80px] mb-1">
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home1.jpg"
+            sizeW={200}
+            text="ATHENES 11x15
+Acrylics on paper"
+          />
+          <ImageClickHover
+            padding={true}
+            text="OBSESSION 58X45’ ABSTRACT PAINTING ON ACRYLICS"
+            source="/homecollage/home2.jpg"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home3.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home4.png"
+            text="Composition 20*93 D\ 19x24’ Abstract Art over acrylics"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home5.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home6.png"
+            text="Composition 20*20 D\ 19x24’ Abstract Art over acrylics on paper"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home7.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home8.jpg"
+            text="ATHENES 11x15
+Acrylics on paper"
+            sizeW={200}
+          />
+        </div>
+        {/* COLUMN 2 */}
+        <div className="md:flex hidden flex-col mt-[125px] mb-10">
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home9.jpg"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            text="WALKING THROUGH THE LINE 19X24’ ACRYLICS OVER PAPER"
+            source="/homecollage/home10.jpg"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home11.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home12.jpg"
+            text="OBSESSION 58X45’ ABSTRACT PAINTING ON ACRYLICS"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home13.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home14.jpg"
+            text="ATHENES 11x15
+Acrylics on paper"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home15.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home16.png"
+            text="MOVEMENT 2020:19X22’ ABSTRACT PAINTING ON ACRYLICS"
+            sizeW={200}
+          />
+        </div>
+        {/* COLUMN 3 */}
+        <div className="md:flex hidden flex-col mt-[60px] mb-10">
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home17.jpg"
+            text="ATHENES 11x15
+Acrylics on paper"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home18.png"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            text="Composition 20*20 D\ 19x24’ Abstract Art over acrylics on paper"
+            source="/homecollage/home19.png"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home20.jpg"
+            text="WALKING THROUGH THE LINE 19X24’ ACRYLICS OVER PAPER"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home21.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home22.jpg"
+            text="ATHENES 11x15
+Acrylics on paper"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home23.png"
+            text="MOVEMENT 2020:19X22’ ABSTRACT PAINTING ON ACRYLICS"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home24.jpg"
+            text=""
+            sizeW={200}
+          />
+        </div>
+        {/* COLUMN 4 */}
+
+        <div className="md:flex hidden flex-col mt-[90px] mb-10">
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home25.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home26.jpg"
+            text="OBSESSION 58X45’ ABSTRACT PAINTING ON ACRYLICS"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            text=""
+            source="/homecollage/home27.jpg"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home28.jpg"
+            text="WALKING THROUGH THE LINE 19X24’ ACRYLICS OVER PAPER"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home29.png"
+            text="Mixed Media 2020:18X24’ ABSTRACT PAINTING ON ACRYLICS"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home29.jpg"
+            text=""
+            sizeW={200}
+          />
+
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home30.png"
+            text="Mixed Media 2020:18X24’ ABSTRACT PAINTING ON ACRYLICS"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home31.jpg"
+            text=""
+            sizeW={200}
+          />
+        </div>
+        {/* COLUMN 5 */}
+        <div className="md:flex hidden flex-col mt-[60px] mb-10">
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home32.png"
+            text="MOVEMENT 2020:19X22’ ABSTRACT PAINTING ON ACRYLICS"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home33.jpg"
+            text="WALKING THROUGH THE LINE 19X24’ ACRYLICS OVER PAPER"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            text="OBSESSION 58X45’ ABSTRACT PAINTING ON ACRYLICS"
+            source="/homecollage/home34.jpg"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home35.jpg"
+            text="PATCHES 56X60’ ACRYLICS OVER CANVAS"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home36.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home37.jpg"
+            text="ATHENES 11x15
+Acrylics on paper"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home38.png"
+            text="Composition 20*20 D\ 19x24’ Abstract Art over acrylics on paper"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home39.jpg"
+            text=""
+            sizeW={200}
+          />
+        </div>
+        {/* COLUMN 6 */}
+        <div className="md:flex hidden flex-col mt-[76px] mb-10">
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home40.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home41.jpg"
+            text="PATCHES 56X60’ ACRYLICS OVER CANVAS"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home42.jpg"
+            text="WALKING THROUGH THE LINE 19X24’ ACRYLICS OVER PAPER"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home43.png"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home44.png"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home45.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home46.jpg"
+            text="ATHENES 11x15
+Acrylics on paper"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home47.png"
+            text="Composition 20*20 D\ 19x24’ Abstract Art over acrylics on paper"
+            sizeW={200}
+          />
+        </div>
+        {/* COLUMN 7 */}
+        <div className="flex flex-col mt-[65px] mb-10">
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home48.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home49.png"
+            text="MOVEMENT 2020:19X22’ ABSTRACT PAINTING ON ACRYLICS"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home50.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home51.jpg"
+            text="OBSESSION 80X60’ ABSTRACT PAINTING ON ACRYLICS"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home52.png"
+            text="Composition 20*93 D\ 19x24’ Abstract Art over acrylics"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home53.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home54.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home55.jpg"
+            text=""
+            sizeW={200}
+          />
+        </div>
+        {/* COLUMN 8 */}
+        <div className="flex flex-col mt-[125px] mb-10">
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home56.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home57.png"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home58.png"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home59.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home60.jpg"
+            text="ATHENES 11x15
+Acrylics on paper"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home61.jpg"
+            text="ATHENES 11x15
+Acrylics on paper"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home62.png"
+            text="Composition 20*93 D\ 19x24’ Abstract Art over acrylics"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home63.jpg"
+            text=""
+            sizeW={200}
+          />
+        </div>
+        {/* COLUMN 9 */}
+        <div className="flex flex-col mt-[60px] mb-10">
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home64.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home65.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home66.jpg"
+            text="It’s a mark- 19x24 Acrylic on paper with gold"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home67.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home68.jpg"
+            text="It’s a mark- 19x24 Acrylic on paper with gold"
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home69.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home70.jpg"
+            text=""
+            sizeW={200}
+          />
+          <ImageClickHover
+            padding={true}
+            source="/homecollage/home71.jpg"
+            text="It’s a mark- 19x24 Acrylic on paper with gold"
+            sizeW={200}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
