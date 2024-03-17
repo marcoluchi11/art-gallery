eeb240aaeac481c2ad7dd230246e0abfb7f8d055
marcoluchi11 <marcoaluchi@hotmail.com>
2024-03-17 15:06:22 -0300

    add photography catalogue

--- components/DownloadButton.jsx
+++ components/DownloadButton.jsx
@@ -3,13 +3,21 @@ import { IoMdDownload } from "react-icons/io";
 
 export default function DownloadButton() {
   return (
-    <div className="flex justify-center my-5 text-md underline md:no-underline md:text-3xl">
+    <div className="flex flex-col items-center justify-center my-5 text-md underline md:no-underline md:text-3xl">
       <Link
         href="https://online.flippingbook.com/view/122448442/?_gl=1*1gs6xv0*_gcl_au*NzIyODY0MDgzLjE3MDg1MzM5Mjk"
         className="flex items-center justify-center hover:underline"
         target="_blank"
       >
         ALBION VU ARTISTIC JOURNEY CATALOGUE
+        <IoMdDownload className="mx-3" />
+      </Link>
+      <Link
+        href="https://online.flippingbook.com/view/891004140/"
+        className="flex items-center justify-center hover:underline mt-5"
+        target="_blank"
+      >
+        ALBION VU PHOTOGRAPHY
         <IoMdDownload className="mx-3" />
       </Link>
     </div>
