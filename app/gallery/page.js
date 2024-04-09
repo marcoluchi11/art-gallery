4aa146aa05d3d3a13326cf06b40c4fae2986c312
marcoluchi11 <marcoaluchi@hotmail.com>
2024-04-08 21:39:51 -0300

    add homecollage to gallery route

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,7 +1,9 @@
+import HomeCollage from "@/components/home/HomeCollage";
+
 export default function Gallery() {
   return (
-    <main className="flex-col items-center  md:px-24 pt-20 mt-[200px] md:pt-0 md:mt-[200px] ">
-      <h1 className="text-2xl font-light">Coming Soon...</h1>
+    <main className="flex-col items-center  md:px-24 pt-20 mt-[200px] md:pt-0 md:mt-[120px] ">
+      <HomeCollage />
     </main>
   );
 }
