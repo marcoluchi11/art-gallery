1265ed7ca0a143814f0a9d99400a5c513e948536
marcoluchi11 <marcoaluchi@hotmail.com>
2024-04-09 22:33:19 -0300

    one under the other line

--- components/home/HomeImages.jsx
+++ components/home/HomeImages.jsx
@@ -12,10 +12,14 @@ export default function HomeImages() {
           source="/nextmove/nextmove1.png"
           text="NEXT MOVE 1 52X62’ ACRYLICS OVER CANVAS"
         />
+      </div>
+      <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
         <ImageClickHover
           source="/nextmove/nextmove2.png"
           text="NEXT MOVE 2 60X60’ ACRYLICS OVER CANVAS"
         />
+      </div>
+      <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
         <ImageClickHover
           source="/nextmove/nextmove3.png"
           text="NEXT MOVE 3 52X62’ ACRYLICS OVER CANVAS"
@@ -26,14 +30,20 @@ export default function HomeImages() {
           source="/nextmove/nextmove4.png"
           text="NEXT MOVE 4 62X66’ ACRYLICS OVER CANVAS"
         />
+      </div>
+      <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
         <ImageClickHover
           source="/nextmove/nextmove5.png"
           text="NEXT MOVE 5 52X59’ ACRYLICS OVER CANVAS"
         />
+      </div>
+      <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
         <ImageClickHover
           source="/nextmove/nextmove6.png"
           text="NEXT MOVE 6 57X57’ ACRYLICS OVER CANVAS"
         />
+      </div>
+      <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
         <ImageClickHover
           source="/nextmove/nextmove7.png"
           text="NEXT MOVE 7 62X82’ ACRYLICS OVER CANVAS"
@@ -44,37 +54,41 @@ export default function HomeImages() {
           source="/nextmove/nextmove8.png"
           text="NEXT MOVE 8 45X45’ ACRYLICS OVER CANVAS"
         />
+      </div>
+      <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
+        {" "}
         <ImageClickHover
           source="/nextmove/nextmove9.png"
           text="NEXT MOVE 9 45X45’ ACRYLICS OVER CANVAS"
         />
-
+      </div>
+      <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
         <ImageClickHover
           source="/nextmove/nextmove10.png"
           text="NEXT MOVE 10 45X45’ ACRYLICS OVER CANVAS"
         />
+      </div>
 
-        <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
-          <ImageClickHover
-            source="/nextmove/nextmove11.png"
-            text="NEXT MOVE 11 62X82’ ACRYLICS OVER CANVAS"
-            sizeW={600}
-          />
-        </div>
-        <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
-          <ImageClickHover
-            source="/nextmove/nextmove12.png"
-            text="NEXT MOVE 12 100X47’ ACRYLICS OVER CANVAS"
-            sizeW={600}
-          />
-        </div>
-        <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
-          <ImageClickHover
-            source="/nextmove/nextmove13.png"
-            text="NEXT MOVE 13 112X62’ ACRYLICS OVER CANVAS"
-            sizeW={600}
-          />
-        </div>
+      <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
+        <ImageClickHover
+          source="/nextmove/nextmove11.png"
+          text="NEXT MOVE 11 62X82’ ACRYLICS OVER CANVAS"
+          sizeW={600}
+        />
+      </div>
+      <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
+        <ImageClickHover
+          source="/nextmove/nextmove12.png"
+          text="NEXT MOVE 12 100X47’ ACRYLICS OVER CANVAS"
+          sizeW={600}
+        />
+      </div>
+      <div className="flex flex-wrap md:flex-row gap-5  w-full mt-10">
+        <ImageClickHover
+          source="/nextmove/nextmove13.png"
+          text="NEXT MOVE 13 112X62’ ACRYLICS OVER CANVAS"
+          sizeW={600}
+        />
       </div>
     </>
   );
