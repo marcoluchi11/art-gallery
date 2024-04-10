1f4fc4fe37b9065d8dd48e484312c3a4f34e4980
marcoluchi11 <marcoaluchi@hotmail.com>
2024-04-10 14:18:03 -0300

    add uppercase letter

--- components/Description.jsx
+++ components/Description.jsx
@@ -1,3 +1,5 @@
 export default function Description({ text }) {
-  return <p className="text-md font-light mx-10 leading-8 mt-10">{text}</p>;
+  return (
+    <p className="uppercase text-lg font-light mx-10 leading-8 mt-10">{text}</p>
+  );
 }
