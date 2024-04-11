3b95b43a19e5d02082bc6221da4bd85cc30e9818
marcoluchi11 <marcoaluchi@hotmail.com>
2024-04-11 16:25:04 -0300

    add component for small tag

---
+++ components/ClickLargerFormat.jsx
@@ -0,0 +1,7 @@
+export default function ClickLargerFormat() {
+  return (
+    <small className="flex justify-center text-center mt-10 uppercase text-xs text-black">
+      CLICK THE IMAGES TO SEE THE ART PIECES ON A LARGER FORMAT
+    </small>
+  );
+}
