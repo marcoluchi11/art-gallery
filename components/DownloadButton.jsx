6828131ad0a5585894d06edb3bc14b2c0430b99e
marcoluchi11 <marcoaluchi@hotmail.com>
2024-04-11 16:53:12 -0300

    add text prop resulting in bigger text on homepage

--- components/DownloadButton.jsx
+++ components/DownloadButton.jsx
@@ -3,13 +3,12 @@ import { IoMdDownload } from "react-icons/io";
 
 export default function DownloadButton({ text = "text-3xl" }) {
   return (
-    // CENTRA LOS TEXTOS PERO EN UN CSOTADO
     <div
-      className={`flex flex-col ml-2 md:ml-0 justify-center text-justify my-5 text-md underline md:no-underline md:${text}`}
+      className={`flex flex-col ml-2 md:ml-0 justify-center text-justify my-5 underline md:no-underline text-md md:${text}`}
     >
       <Link
         href="https://online.flippingbook.com/view/122448442/?_gl=1*1gs6xv0*_gcl_au*NzIyODY0MDgzLjE3MDg1MzM5Mjk"
-        className="flex items-center  hover:underline"
+        className={`flex items-center  hover:underline ${text}`}
         target="_blank"
       >
         ALBION VU ARTISTIC JOURNEY CATALOGUE
@@ -17,7 +16,7 @@ export default function DownloadButton({ text = "text-3xl" }) {
       </Link>
       <Link
         href="https://online.flippingbook.com/view/891004140/"
-        className="flex items-center hover:underline mt-5"
+        className={`flex items-center hover:underline mt-5 ${text}`}
         target="_blank"
       >
         ALBION VU PHOTOGRAPHY
--- components/home/HomePage.jsx
+++ components/home/HomePage.jsx
@@ -5,11 +5,11 @@ export default function Homepage() {
     <>
       <div className="max-w-[1366px]">
         <div className="mb-20">
-          <DownloadButton />
+          <DownloadButton text="text-3xl" />
           <HomeImages />
         </div>
 
-        <DownloadButton />
+        <DownloadButton text="text-3xl" />
       </div>
     </>
   );
