ed9c38db91c3ddadb4c54c117e2e8914ac81a0f1
marcoluchi11 <marcoaluchi@hotmail.com>
2024-04-26 16:54:02 -0300

    add text prop

--- components/DownloadButton.jsx
+++ components/DownloadButton.jsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 import { IoMdDownload } from "react-icons/io";
 
-export default function DownloadButton({ text = "text-3xl" }) {
+export default function DownloadButton({ text = "text-2xl" }) {
   return (
     <div
-      className={`flex flex-col ml-2 md:ml-0 justify-center text-justify my-5 underline md:no-underline text-md md:${text}`}
+      className={`flex flex-col ml-2 md:ml-0 justify-center text-justify my-5 underline md:no-underline text-md `}
     >
       <Link
         href="https://online.flippingbook.com/view/122448442/?_gl=1*1gs6xv0*_gcl_au*NzIyODY0MDgzLjE3MDg1MzM5Mjk"
-        className={`flex items-center  hover:underline ${text}`}
+        className={`flex items-center  hover:underline text-md  md:${text}`}
         target="_blank"
       >
         ALBION VU ARTISTIC JOURNEY CATALOGUE
@@ -16,7 +16,7 @@ export default function DownloadButton({ text = "text-3xl" }) {
       </Link>
       <Link
         href="https://online.flippingbook.com/view/891004140/"
-        className={`flex items-center hover:underline mt-5 ${text}`}
+        className={`flex items-center hover:underline mt-5 text-md md:${text}`}
         target="_blank"
       >
         ALBION VU PHOTOGRAPHY
